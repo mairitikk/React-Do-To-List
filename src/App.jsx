86a373944885211e048923c6fa6eed9a9e5e3013
mairi tikk <mairi.tikk@gmail.com>
2024-10-08 11:57:47 +0200

    add basic html code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-
-
+import { useState } from "react"
+import "./styles.css"
 
 export default function App() {
-  return "Tere"
+  useState()
+  return <>
+    
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" id="item1" />
+          Item1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </>
 }
